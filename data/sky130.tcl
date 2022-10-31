b2a8fdc825f6173f5c9fd3541600586f49255b27
Olof Kindgren <olof.kindgren@gmail.com>
2022-10-31 01:01:11 +0100

    Add FuseSoC support and Github CI actions
    This adds a core description file for the $core core that exposes targets
    for linting, the testbench and for building a GDSII using OpenLANE. All targets are also
    implemented as Github actions so that they get run on every push to the repo.
    
    Quick FuseSoC instructions:
    
     #install FuseSoC
    pip3 install fusesoc
     #Create and enter a new workspace
    mkdir workspace && cd workspace
     #Register $core as a library in the workspace
    fusesoc library add $core /path/to/$core
     #...if repo is available locally or...
    fusesoc library add $core https://github.com/$repo
     #...to get the upstream repo
    
     #To run lint
    fusesoc run --target=lint $vlnv
    
     #Run testbench
    fusesoc run --target=sim xtea
     #Run with modelsim instead of default tool (icarus)
    fusesoc run --target=sim --tool=modelsim xtea
    
    EDALIZE_LAUNCHER=el_docker fusesoc run --target=sky130 $vlnv
     #List all targets
    fusesoc core show $vlnv

---
+++ data/sky130.tcl
@@ -0,0 +1,9 @@
+set ::env(CLOCK_PORT) "clock"
+set ::env(CLOCK_NET) "clock"
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(GLB_RT_ADJUSTMENT) 0.15
+
+set ::env(CLOCK_PERIOD) "26.03"
+set ::env(FP_CORE_UTIL) 45
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
